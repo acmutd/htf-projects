f200d25488a3bc8768094d0c2bd122615581ef88
Tu Chu <tu.chu@my.tccd.edu>
2020-10-17 14:07:28 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 // Make sure you add an import for your card!
-import ACMCard from './components/ACMCard';
+import Tu from './components/Tu';
 
 function App() {
   return (
@@ -16,7 +16,8 @@ function App() {
 
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
-        <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
+        <Tu name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co" />
+        <Tu name="Tu Chu" email="tu.chu@utdallas.edu" phone="123-456-7890" address="800 West Campbell Road" website="N/A" />
 
         {/*Add your component above this comment to put it into the app!*/}
       </div>
