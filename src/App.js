9d632c112f54adf2721113501194ce4ea726e2d5
Nathan Adam <36092204+Schobbish@users.noreply.github.com>
2020-10-17 14:54:50 -0500

    add schobbish card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Make sure you add an import for your card!
 import ACMCard from './components/ACMCard';
+import SchobbishCard from './components/SchobbishCard';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
-
+        <SchobbishCard name="Nathan Adam" email="nla190001@utdallas.edu" phone="867-5309" address="2801 Rutford Ave" website="https://a.schobbish.com/in"/>
         {/*Add your component above this comment to put it into the app!*/}
       </div>
     </div>
