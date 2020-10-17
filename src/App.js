73aadec3dcae0e3e9393582245699582af897999
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2020-10-17 14:12:37 -0500

    Add business card for Liz Wigglesworth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Make sure you add an import for your card!
 import ACMCard from './components/ACMCard';
+import LizW from './components/LizW';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
-
+        <LizW name="Liz Wigglesworth" github="lizgw" email="liz.wigglesworth@gmail.com" website="lizgw.github.io"/>
         {/*Add your component above this comment to put it into the app!*/}
       </div>
     </div>
