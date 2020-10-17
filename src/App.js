d91fd5460233b3e4d2c74ff2f9105701ffb61b53
Gagan Bhat <gaganbhat6@gmail.com>
2020-10-17 13:45:04 -0500

    Added my basic card for HTF PR

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 
 // Make sure you add an import for your card!
+import GaganBhatCard from './components/GaganBhatCard';
 import ACMCard from './components/ACMCard';
+
 
 function App() {
   return (
@@ -17,7 +19,7 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
-
+        <GaganBhatCard name="Gagan Bhat" email="gaganbhat6@gmail.com" phone="XXX-XXX-XXXX" address="2501 Ohio Dr. Plano, TX" website="http://gaganbhat.me/"/>
         {/*Add your component above this comment to put it into the app!*/}
       </div>
     </div>
