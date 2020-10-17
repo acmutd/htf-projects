7b494490874c8aa7bf8f75daea02d62e0cd6e949
amrutha-bhargavi <amruthabhargavi1995@gmail.com>
2020-10-17 14:06:38 -0500

    Amrutha's Card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Make sure you add an import for your card!
 import ACMCard from './components/ACMCard';
+import AmruthaRCard from './components/AmruthaRCard'
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
-
+        <AmruthaRCard name="Amrutha Bhargavi Rajkumar" email="amruthabhargavi95@gmail.com" phone="996-5257" address="4721 Christopher Court" website="https://amrutha-bhargavi.github.io/personal-portfolio/"/>
         {/*Add your component above this comment to put it into the app!*/}
       </div>
     </div>
