2049a10201cbb2403589ac9ed9f7c78e03af4b12
Travis A. Dula <travisadula@gmail.com>
2020-10-17 13:47:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Make sure you add an import for your card!
 import ACMCard from './components/ACMCard';
+import TravisD from './components/TravisD';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
+        <TravisD name="Travis A. Dula" email="travisadula@gmail.com" phone="9729879679" address="Plano, TX" website="https://www.linkedin.com/in/travis-dula/"/>
 
         {/*Add your component above this comment to put it into the app!*/}
       </div>
