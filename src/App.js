a4847608beac8bc35b08ad2db4248997b1db7c36
peilune <peiyun.t@gmail.com>
2020-10-17 19:03:45 -0500

    updated info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 
 // Make sure you add an import for your card!
-import ACMCard from './components/ACMCard';
+import ACMCard from './components/ACMCard'
+import PeiyunT from './components/PeiyunT';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
       <div id="business-cards">
         {/*Notice how you can give named attributes to your component!*/}
         <ACMCard name="Association for Computing Machinery" email="contact@acmutd.co" phone="555-1234" address="800 West Campbell Road" website="acmutd.co"/>
+
+        <PeiyunT name="Pei-Yun Tseng" email="pxt190024@utdallas.edu" phone="512-206-6162" address="1600 Amelia Court" website="acmutd.co"/>
 
         {/*Add your component above this comment to put it into the app!*/}
       </div>
